bf77bea46647d131965f15dec5d1395f553a5893
sivakumar3442 <sivakumar.sf3442@gmail.com>
2022-08-09 22:05:12 +0530

    sample updated

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CoreFileManager.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Newtonsoft.Json;
+using Syncfusion.EJ2.FileManager.AmazonS3FileProvider;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using Syncfusion.EJ2.FileManager.Base;
+using Amazon;
+
+
+namespace CoreFileManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class FileManagerDirectoryContent1 : FileManagerDirectoryContent
+        {
+            public string RootName { get; set; }
+        }
+        public AmazonS3FileProvider operation;
+        public string basePath;
+        protected RegionEndpoint bucketRegion;
+        public HomeController(Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment)
+        {
+            this.basePath = hostingEnvironment.ContentRootPath;
+            this.operation = new AmazonS3FileProvider();           
+            this.operation.RegisterAmazonS3("<---bucketName--->", "<---awsAccessKeyId--->", "<---awsSecretAccessKey--->", "<---region--->");  
+        }
+        public object AmazonS3FileOperations([FromBody] FileManagerDirectoryContent1 args)
+        {
+            this.operation.setRootName(args.RootName);
+            if (args.Action == "delete" || args.Action == "rename")
+            {
+                if ((args.TargetPath == null) && (args.Path == ""))
+                {
+                    FileManagerResponse response = new FileManagerResponse();
+                    ErrorDetails er = new ErrorDetails
+                    {
+                        Code = "401",
+                        Message = "Restricted to modify the root folder."
+                    };
+                    response.Error = er;
+                    return this.operation.ToCamelCase(response);
+                }
+            }
+            switch (args.Action)
+            {
+                case "read":
+                    // reads the file(s) or folder(s) from the given path.
+                    return this.operation.ToCamelCase(this.operation.GetFiles(args.Path, false, args.Data));
+                case "delete":
+                    // deletes the selected file(s) or folder(s) from the given path.
+                    return this.operation.ToCamelCase(this.operation.Delete(args.Path, args.Names, args.Data));
+                case "copy":
+                    // copies the selected file(s) or folder(s) from a path and then pastes them into a given target path.
+                    return this.operation.ToCamelCase(this.operation.Copy(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData, args.Data));
+                case "move":
+                    // cuts the selected file(s) or folder(s) from a path and then pastes them into a given target path.
+                    return this.operation.ToCamelCase(this.operation.Move(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData, args.Data));
+                case "details":
+                    // gets the details of the selected file(s) or folder(s).
+                    return this.operation.ToCamelCase(this.operation.Details(args.Path, args.Names, args.Data));
+                case "create":
+                    // creates a new folder in a given path.
+                    return this.operation.ToCamelCase(this.operation.Create(args.Path, args.Name, args.Data));
+                case "search":
+                    // gets the list of file(s) or folder(s) from a given path based on the searched key string.
+                    return this.operation.ToCamelCase(this.operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive, args.Data));
+                case "rename":
+                    // renames a file or folder.
+                    return this.operation.ToCamelCase(this.operation.Rename(args.Path, args.Name, args.NewName, false, args.Data));
+            }
+            return null;
+        }
+
+        // uploads the file(s) into a specified path
+        public IActionResult AmazonS3Upload(string path, IList<IFormFile> uploadFiles, string action, string data, string rootName)
+        {
+            this.operation.setRootName(rootName);
+            FileManagerResponse uploadResponse;
+            FileManagerDirectoryContent[] dataObject = new FileManagerDirectoryContent[1];
+            dataObject[0] = JsonConvert.DeserializeObject<FileManagerDirectoryContent>(data);
+            uploadResponse = operation.Upload(path, uploadFiles, action, dataObject);
+            if (uploadResponse.Error != null)
+            {
+                Response.Clear();
+                Response.ContentType = "application/json; charset=utf-8";
+                Response.StatusCode = Convert.ToInt32(uploadResponse.Error.Code);
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = uploadResponse.Error.Message;
+            }
+            return Content("");
+        }
+
+        // downloads the selected file(s) and folder(s)
+        public IActionResult AmazonS3Download(string downloadInput)
+        {
+            FileManagerDirectoryContent1 args = JsonConvert.DeserializeObject<FileManagerDirectoryContent1>(downloadInput);
+            this.operation.setRootName(args.RootName);
+            return operation.Download(args.Path, args.Names);
+        }
+
+        // gets the image(s) from the given path
+        public IActionResult AmazonS3GetImage(FileManagerDirectoryContent1 args)
+        {
+            this.operation.setRootName(args.RootName);
+            return operation.GetImage(args.Path, args.Id, false, null, args.Data);
+        }
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+       
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
